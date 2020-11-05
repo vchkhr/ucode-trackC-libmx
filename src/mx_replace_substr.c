f475f12af5aa40f6537724a7497c699077c56a6f
Viacheslav Kharchenko <vchkhr@gmail.com>
2020-11-05 14:15:59 +0200

    Fix some files

--- src/mx_replace_substr.c
+++ src/mx_replace_substr.c
@@ -5,17 +5,15 @@ char *mx_replace_substr(const char *str, const char *sub, const char *replace) {
 	int len = mx_strlen(str) + mx_count_substr(str, sub) * sum;
 	char *memory = mx_strnew(len);
 
-	if (!str || !sub || !replace || mx_strlen(str) <= mx_strlen(sub)) {
+	if (!str || !sub || !replace || mx_strlen(str) <= mx_strlen(sub))
 		return NULL;
-    }
 
 	for (int i = 0; i < len; i++, str++) {
         if (!mx_strncmp((char *)str, (char *)sub, mx_strlen(sub))) {
             str += mx_strlen(sub);
 
-            for (int j = 0; j < mx_strlen(replace); i++, j++) {
+            for (int j = 0; j < mx_strlen(replace); i++, j++)
                 memory[i] = replace[j];
-            }
         }
 
         memory[i] = *str;
